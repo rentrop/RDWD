e3b4596b2520212825b051f9c016b6813a9ae3cb
Richard Rentrop <rrichard@ip-172-31-1-183.eu-central-1.compute.internal>
2014-11-17 12:33:12 +0000

    write_dwd_s3 now creates zip-archives

--- R/write_dwd.R
+++ R/write_dwd.R
@@ -17,3 +17,20 @@ write_dwd_s3 <- function(dat){
                paste0("s3://dwd-oekoproj/station-dat/",filename)
   ))
 }
+
+write_dwd_s3 <- function(dat, zip = TRUE){
+  filename = paste0("station",dat[,head(station_id,1)])
+  write.csv(dat, file=paste0(getwd(),"/",filename,".csv"), row.names = FALSE)
+  zip(paste0(getwd(),"/",filename,".tar.gz"), paste0(getwd(),"/",filename,".csv"))
+  
+  if (zip){
+    file = paste0(getwd(),"/",filename,".tar.gz")
+  } else {
+    file = paste0(getwd(),"/",filename,".csv")
+  }
+
+  system(paste("aws s3 mv", 
+               file, 
+               paste0("s3://dwd-oekoproj/station-dat/")
+  ))
+}
